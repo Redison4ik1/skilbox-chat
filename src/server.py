613e05d18749e3da2d02016d03163c82af845ef3
Den P <redis1@ya.ru>
2019-10-09 09:48:04 +0300

    homework for skillbox

--- src/server.py
+++ src/server.py
@@ -48,11 +48,10 @@ class Handler(LineOnlyReceiver):
         if self.login is not None:
             # формируем сообщение для пересылки другим клиентам
             message = f"<{self.login}>: {message}"
-
+            self.factory.add_history(message)
             # циклически отправляем остальным клиентам (кроме текущего) TODO: смотри `send_message_to_client`
-            for user in self.factory.clients:
-                if user is not self:
-                    user.sendLine(message.encode())  # кодируем снова строку в байты
+            self.factory.send_message_to_client(self, message)
+
         # если у клиента еще нет логина (первый раз зашел)
         else:
             # проверяем, что прислал правильную команду (например - login:admin)
@@ -62,14 +61,18 @@ class Handler(LineOnlyReceiver):
 
                 # записываем логин
                 self.login = login
-
                 # TODO: пометка для ДЗ (задание №1)
-
-                # выводим уведомление в консоль
-                print(f"New user: {login}")
-                # отправляем клиенту приветственное сообщение
-                self.sendLine("Welcome!!!".encode())
-                # TODO: пометка для ДЗ (задание №2)
+                logins = [user.login for user in self.factory.clients if user is not self]
+                if self.login in logins:
+                    self.sendLine(f"Логин {login} занят, попробуйте другой".encode())
+                    self.transport.loseConnection()
+                else:
+                    # выводим уведомление в консоль
+                    print(f"New user: {login}")
+                    # отправляем клиенту приветственное сообщение
+                    self.sendLine("Welcome!!!".encode())
+                    # TODO: пометка для ДЗ (задание №2)
+                    self.factory.send_history(self)
             # если прислал неправильную команду
             else:
                 # отправим клиенту текст с ошибкой
@@ -83,6 +86,8 @@ class Server(ServerFactory):
 
     protocol = Handler  # тип протокола для подключения
     clients: list  # список активных клиентов
+    history: list = []
+    _history_length: int = 10
 
     def __init__(self):
         """
@@ -97,23 +102,30 @@ class Server(ServerFactory):
         """
         print("Server started...")
 
-    def send_message_to_client(self, message):
+    def send_message_to_client(self, current_client, message):
         """
         Метод для отправки сообщений всем клиентам
         :param message:
         :return:
         """
-        # TODO: сюда можно вынести код со строк 52-55
-        pass
+        for client in self.clients:
+            if client is not current_client:
+                client.sendLine(message.encode())  # кодируем снова строку в байты
 
-    def send_history(self, count=10):
+    def send_history(self, client):
         """
         Метод для отправки истории
         :param count:
         :return:
         """
         # TODO: пометка для ДЗ (задание №2)
-        pass
+        if self.history:
+            client.sendLine('\n'.join(self.history).encode())
+
+    def add_history(self, message):
+        if len(self.history) >= self._history_length:
+            del self.history[0]
+        self.history.append(message)
 
 
 # указание конфигурации реактора (порт и тип сервера)
